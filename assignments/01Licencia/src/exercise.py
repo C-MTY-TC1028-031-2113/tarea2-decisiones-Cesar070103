009fe1911037931727c09550e93b580fa46b4abf
Cesar070103 <a01285056@itesm.mx>
2021-08-28 23:49:12 -0500

    290821

--- assignments/01Licencia/src/exercise.py
+++ assignments/01Licencia/src/exercise.py
@@ -1,7 +1,14 @@
 
 def main():
-    pass
-
+    edad = int(input("Ingresa tu edad: "))
+    if edad >= 18:
+        oficial = str(input("¿Tienes identificación oficial? (s/n) "))
+        if oficial == "s":
+            print("Trámite de licencia concedido")
+        else:
+            print("No cumples requisitos")
+    else:
+        print("No cumples requisitos")
 
 if __name__ == '__main__':
     main()
